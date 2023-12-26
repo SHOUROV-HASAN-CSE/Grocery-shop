7ca959999c1fdfb7d319727b2b0d703e230bc99f
SHOUROV HASAN <108481592+SHOUROV-HASAN-CSE@users.noreply.github.com>
2023-12-26 21:11:59 +0600

    new Section Created

--- app/(global)/category-details/page.jsx
+++ app/(global)/category-details/page.jsx
@@ -29,9 +29,55 @@ const CategoryPage = () => {
 
       {/* main section */}
       <div className="grid grid-cols-12 gap-4">
-        <div className="col-span-3 rounded-md bg-[#fff] mr-2 p-6 mb-2 ">
+        <div className="col-span-3 ">
+          {/* section 1 */}
+          <div className="rounded-md bg-[#fff] p-6 mb-2">
           <p className="pb-20">Price Range</p>
           <div><input type="range" min={0} max="100" value="10" className="range range-error" /></div>
+          </div>
+          {/* section 2 */}
+          <div className="rounded-md bg-[#fff] p-6 mb-2">
+          <p className="pb-6">Availabilty</p>
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <input type="checkbox" checked="checked" className="checkbox" />
+              <span className="label-text">In Stock</span> 
+            </label>
+            <label className="label cursor-pointer">
+              <input type="checkbox" checked="checked" className="checkbox" />
+              <span className="label-text">Pre Order</span> 
+            </label>
+            <label className="label cursor-pointer">
+              <input type="checkbox" checked="checked" className="checkbox" />
+              <span className="label-text">Up Coming</span> 
+            </label>
+          </div>
+          </div>
+          {/* section 3 */}
+          <div className="rounded-md bg-[#fff] p-6 mb-2">
+          <p className="pb-6">Printer Type</p>
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <input type="checkbox" checked="checked" className="checkbox" />
+              <span className="label-text">Inkjet</span> 
+            </label>
+            <label className="label cursor-pointer">
+              <input type="checkbox" checked="checked" className="checkbox" />
+              <span className="label-text">Ink Tank</span> 
+            </label>
+            <label className="label cursor-pointer">
+              <input type="checkbox" checked="checked" className="checkbox" />
+              <span className="label-text">Laser</span> 
+            </label>
+            <label className="label cursor-pointer">
+              <input type="checkbox" checked="checked" className="checkbox" />
+              <span className="label-text">Dotmatrix</span> 
+            </label>
+          </div>
+          </div>
+
+
+
         </div>
 
         <div className="col-span-9 rounded-md bg-[#fff] ">
