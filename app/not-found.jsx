1744d53f7a815a113d3c0a723a78d496f7d00085
Najmul <najmulbinnurul@gmail.com>
2023-12-27 14:35:07 +0600

    not found image alt

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -4,13 +4,13 @@ import notFound from '../public/not-found.svg';
 const ErrorPage = () => {
   return (
     <div className='flex h-screen w-full flex-col items-center justify-center'>
-      <Image src={notFound}></Image>
+      <Image src={notFound} alt=''></Image>
       <h3 className='my-3 text-center text-2xl font-bold text-blue-500'>
         Oops!
       </h3>
       <p>The page you requested cannot be found.</p>
       <Link href='/'>
-        <button class='rounded hover:bg-blue-500 px-4 py-2 font-bold text-white bg-blue-700 mt-5'>
+        <button class='mt-5 rounded bg-blue-700 px-4 py-2 font-bold text-white hover:bg-blue-500'>
           Return Home
         </button>
       </Link>
