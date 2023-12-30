6cfebe10deb89d85d170bda045676736ea5d8035
Mohammad Ibrahim <ibinfo2017bd@gmail.com>
2023-12-31 00:49:33 +0600

    vandor page add

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -2,17 +2,19 @@
 import { useEffect, useState } from 'react';
 import DashboardNavbar from './_component/navbar/DashboardNavbar';
 import Sidebar from './_component/sidebar/Siderbar';
-
 export default function GlobalLayout({ children }) {
   const [isVisible, setIsVisible] = useState(true);
-
-  const [darkMode, setDarkMode] = useState(() => {
-    const storedDarkMode = localStorage.getItem('darkMode');
-    return storedDarkMode ? JSON.parse(storedDarkMode) : true; 
-  });
+  const [darkMode, setDarkMode] = useState(true); 
 
   useEffect(() => {
- 
+    const storedDarkMode = localStorage.getItem('darkMode');
+    if (typeof storedDarkMode !== 'undefined' && storedDarkMode !== null) {
+   
+      setDarkMode(JSON.parse(storedDarkMode));
+    }
+  }, []); 
+
+  useEffect(() => {
     if (darkMode) {
       document.documentElement.classList.add('dark');
     } else {
@@ -22,19 +24,23 @@ export default function GlobalLayout({ children }) {
   }, [darkMode]);
 
   const toggleDarkMode = () => {
-    setDarkMode(prevDarkMode => !prevDarkMode);
+    setDarkMode((prevDarkMode) => !prevDarkMode);
   };
+
   const toggleVisibility = () => {
     setIsVisible((prevState) => !prevState);
   };
 
   return (
     <div className='false flex h-screen bg-gray-50 dark:bg-gray-900 '>
-       
       {isVisible && <Sidebar isVisible={isVisible} />}
 
       <div className='flex w-full flex-1 flex-col'>
-        <DashboardNavbar toggleVisibility={toggleVisibility} toggleDarkMode={toggleDarkMode} />
+        <DashboardNavbar
+          toggleVisibility={toggleVisibility}
+          toggleDarkMode={toggleDarkMode}
+        />
+       
         <main>{children}</main>
       </div>
     </div>
